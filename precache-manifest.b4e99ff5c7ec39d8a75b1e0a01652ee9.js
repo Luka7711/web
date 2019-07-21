deead37509bb2647e87005908b38c1b60c751442
Ulukbek Nusubaliev <ulukbeknusubaliev@Ulukbeks-MacBook-Air.local>
2019-07-21 15:35:18 -0500

    Updates

---
+++ precache-manifest.b4e99ff5c7ec39d8a75b1e0a01652ee9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9916783a33d8768e81cb8a66a69d000e",
+    "url": "/web/index.html"
+  },
+  {
+    "revision": "903ab22810aee2d00b4b",
+    "url": "/web/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/web/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "903ab22810aee2d00b4b",
+    "url": "/web/static/js/main.476e293b.chunk.js"
+  },
+  {
+    "revision": "33ed4ec846e9d84e8327",
+    "url": "/web/static/js/runtime~main.c75ffd02.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/web/static/media/logo.5d5d9eef.svg"
+  }
+]);
